a48fa7d91bff64ddc236e9e5e98786102be0974a
miltongiovanni <miltongiovanni@gmail.com>
2023-01-08 07:22:46 -0500

    Edicion, activacion y desctivacion de usuarios

--- src/Controller/UsuariosController.php
+++ src/Controller/UsuariosController.php
@@ -52,15 +52,47 @@ class UsuariosController extends AbstractController
     }
 
     #[Route('/usuario/{id}/editar', name: 'user_edit', methods: ['GET'])]
-    public function edit(Request $request, int $id, UserRepository $userRepository): Response
+    public function edit(Request $request, int $id, UserRepository $userRepository, PerfilesRepository $perfilesRepository): Response
     {
-        $currentUser = $this->getUser($id);
+        $perfiles = $perfilesRepository->findAll();
         $user = $userRepository->find($id);
 
         return $this->render('usuarios/edit.html.twig', [
             'user' => $user,
+            'perfiles' => $perfiles,
             'action' => 'update',
         ]);
+    }
+
+    #[Route('/usuario/{id}/activar', name: 'user_activate', methods: ['GET'])]
+    public function activate(Request $request, int $id, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+        $user->setActivo(true);
+        $entityManager->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Usuario activado correctamente');
+        //$this->addFlash('error', ' Error al actualizar el Usuario');
+        return $this->redirectToRoute('usuarios', [], Response::HTTP_SEE_OTHER);
+
+    }
+    #[Route('/usuario/{id}/desactivar', name: 'user_deactivate', methods: ['GET'])]
+    public function deactivate(Request $request, int $id, EntityManagerInterface $entityManager, UserRepository $userRepository): Response
+    {
+        $user = $userRepository->find($id);
+        $user->setActivo(false);
+        $entityManager->persist($user);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Usuario desactivado correctamente');
+        //$this->addFlash('error', ' Error al actualizar el Usuario');
+        return $this->redirectToRoute('usuarios', [], Response::HTTP_SEE_OTHER);
+
     }
 
     #[Route('/usuario/{id}/actualizar', name: 'user_update', methods: ['POST'])]
@@ -74,6 +106,7 @@ class UsuariosController extends AbstractController
         $action = $request->request->get('action');
         $user->setEmail($request->request->get('email'));
         $perfilValue = $request->request->get('role');
+        $activo = $request->request->get('activo', false);
         $perfilArray = explode('-', $perfilValue);
         $perfil = $perfilesRepository->find($perfilArray[0]);
         $user->setRoles([$perfilArray[1]]);
@@ -90,7 +123,7 @@ class UsuariosController extends AbstractController
             }
         }
         $user->setIsVerified(false);
-        $user->setActivo(false);
+        $user->setActivo($activo);
         $entityManager->persist($user);
 
         // actually executes the queries (i.e. the INSERT query)
